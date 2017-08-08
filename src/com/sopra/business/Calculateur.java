e2c10e4b45b2fc890d19ade61191699e6d102969
Unknown <charles.brebion@gmail.com>
2017-08-08 16:00:01 +0200

    Modif Calculateur

--- src/com/sopra/business/Calculateur.java
+++ src/com/sopra/business/Calculateur.java
@@ -1,5 +1,31 @@
 package com.sopra.business;
 
-public class Calculateur {
+import java.util.List;
 
+import com.sopra.model.Carte;
+
+public class Calculateur {
+	public int calculScoreManche(List<Carte> listCartes, String atout, boolean dixDeDer) {
+		int score = 0;
+		
+		// Parcours des cartes pour calculer leur valeur
+		for (Carte carte : listCartes) {
+			switch (carte.getValeur()) {
+			// 9
+			case 2:
+				// Atout
+				if (carte.getSigne().equals(atout)) {
+					score += 14;
+				}
+				// Pas atout
+				else {
+					score += 0;
+				}
+				break;
+			//10
+			}
+		}
+		
+		return score;
+	}
 }
