a75cad5c02488d88c7ad9559f9ed74c08d47b90f
gonzagueducrocq <gonzague.ducrocq@soprasteria.com>
2017-08-08 16:47:01 +0200

    Auto stash before merge of "Front" and "origin/Front"

--- src/com/sopra/windows/MainWindow.java
+++ src/com/sopra/windows/MainWindow.java
@@ -3,6 +3,7 @@
  */
 package com.sopra.windows;
 
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
@@ -16,6 +17,8 @@ public class MainWindow extends JFrame {
 	Panel2 panelMilieu= new Panel2();
 	Panel3 panelBas= new Panel3();
 	Panel4 panelGauche= new Panel4();
+	
+	JCheckBox checkBox = new JCheckBox();
 	//creation de la checkbox
 	private JPanel panelDroit = new JPanel();
 	
@@ -25,10 +28,11 @@ public class MainWindow extends JFrame {
 		this.setLayout(new FlowLayout());
 		this.add(this.panelGauche);
 		this.add(this.panelDroit);
-		this.panelDroit.setLayout(new BorderLayout());
-		this.panelDroit.add(this.panelHaut, BorderLayout.NORTH);
-		this.panelDroit.add(this.panelMilieu, BorderLayout.CENTER);
-		this.panelDroit.add(this.panelBas, BorderLayout.SOUTH);
+		this.panelDroit.setLayout(new GridLayout(4, 1));
+		this.panelDroit.add(this.panelHaut);
+		this.panelDroit.add(this.panelMilieu);
+		this.panelDroit.add(checkBox);
+		this.panelDroit.add(this.panelBas);
 		
 
 		//Affichage de la fenêtre
--- src/com/sopra/windows/Panel3.java
+++ src/com/sopra/windows/Panel3.java
@@ -10,8 +10,8 @@ import javax.swing.JPanel;
 public class Panel3 extends JPanel {
 
 	
-	private JLabel CheckBox = new JLabel("10 de der."); 
-	private JCheckBox checkBox = new JCheckBox();
+	//private JLabel CheckBox = new JLabel("10 de der."); 
+	//private JCheckBox checkBox = new JCheckBox();
 	
 	private JButton BoutonAjouter = new JButton("Ajouter");
 	private JButton BoutonReset = new JButton("Reset");
@@ -19,8 +19,8 @@ public class Panel3 extends JPanel {
 	
 	public void initialize() {
 		this.setLayout(new FlowLayout());
-		this.add(CheckBox);
-		this.add(checkBox);
+		//this.add(CheckBox);
+		//this.add(checkBox);
 		this.add(BoutonAjouter);
 		this.add(BoutonReset);
 		this.add(BoutonCalcul);
