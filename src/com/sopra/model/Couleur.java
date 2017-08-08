a06fc3b15b1cdcc386b6403a50886ac6a164aec4
Guillaume HALLOUCHERIE <guillaume.halloucherie@live.fr>
2017-08-08 15:42:11 +0200

    Création de la classe Couleur
    Création de l'attribut signe, de ses get/setter

--- src/com/sopra/model/Couleur.java
+++ src/com/sopra/model/Couleur.java
@@ -2,4 +2,14 @@ package com.sopra.model;
 
 public class Couleur {
 
+	private String signe;
+
+	public String getSigne() {
+		return signe;
+	}
+
+	public void setSigne(String signe) {
+		this.signe = signe;
+	}
+	
 }
