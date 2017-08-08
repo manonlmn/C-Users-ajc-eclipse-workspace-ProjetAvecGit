97e9d333c4bebb4941df134c4e34a75d06386e71
Manon <lmn.manon@gmail.com>
2017-08-08 16:17:00 +0200

    Ajout des boutons sur la fenetre
    Creation de constructeurs pour panel1

--- src/com/sopra/windows/MainWindow.java
+++ src/com/sopra/windows/MainWindow.java
@@ -12,13 +12,15 @@ import java.awt.*;
  */
 public class MainWindow extends JFrame {
 
-	Panel1 panelHaut= new Panel1();
+	Panel1 panelHaut= new Panel1(); //ajout automatique des boutons dans panel1
 	Panel2 panelMilieu= new Panel2();
 	Panel3 panelBas= new Panel3();
 	Panel4 panelGauche= new Panel4();
 	private JPanel panelDroit = new JPanel();
+	
+	
 	public void initialize() {
-		
+
 		this.setLayout(new FlowLayout());
 		this.add(this.panelGauche);
 		this.add(this.panelDroit);
@@ -26,6 +28,13 @@ public class MainWindow extends JFrame {
 		this.panelDroit.add(this.panelHaut, BorderLayout.NORTH);
 		this.panelDroit.add(this.panelMilieu, BorderLayout.CENTER);
 		this.panelDroit.add(this.panelBas, BorderLayout.SOUTH);
+		
+
+		//Affichage de la fenêtre
+		this.setSize(800, 400);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 }
--- src/com/sopra/windows/Panel1.java
+++ src/com/sopra/windows/Panel1.java
@@ -1,16 +1,29 @@
 package com.sopra.windows;
 
 import javax.swing.JButton;
+import java.awt.*;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panel1 extends JPanel {
 	
-	private JLabel Atout = new JLabel("Atout");
+	private JLabel Atout = new JLabel("Atout : ");
 	
 	private JButton AtoutPique = new JButton("AtoutPique");
 	private JButton AtoutCoeur = new JButton("AtoutCoeur");
 	private JButton AtoutCarreau = new JButton("AtoutCarreau");
 	private JButton AtoutTrefle = new JButton("AtoutTrefle");
-
+	
+	public void initialize() {
+		this.setLayout(new FlowLayout());
+		this.add(Atout);
+		this.add(AtoutPique);
+		this.add(AtoutCoeur);
+		this.add(AtoutTrefle);
+		this.add(AtoutCarreau);
+	}
+	
+	public Panel1() {
+		this.initialize();
+	}
 }
