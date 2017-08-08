5cfa49ec00ea81d0a7fd51722995a9f9f56258b2
Manon <lmn.manon@gmail.com>
2017-08-08 16:02:31 +0200

    ModificationMainWindow
    Ajout et placement des différents panels

--- src/com/sopra/windows/MainWindow.java
+++ src/com/sopra/windows/MainWindow.java
@@ -4,7 +4,8 @@
 package com.sopra.windows;
 
 import javax.swing.JFrame;
-
+import javax.swing.JPanel;
+import java.awt.*;
 /**
  * @author ajc
  *
@@ -15,9 +16,16 @@ public class MainWindow extends JFrame {
 	Panel2 panelMilieu= new Panel2();
 	Panel3 panelBas= new Panel3();
 	Panel4 panelGauche= new Panel4();
-	
+	private JPanel panelDroit = new JPanel();
 	public void initialize() {
 		
+		this.setLayout(new FlowLayout());
+		this.add(this.panelGauche);
+		this.add(this.panelDroit);
+		this.panelDroit.setLayout(new BorderLayout());
+		this.panelDroit.add(this.panelHaut, BorderLayout.NORTH);
+		this.panelDroit.add(this.panelMilieu, BorderLayout.CENTER);
+		this.panelDroit.add(this.panelBas, BorderLayout.SOUTH);
 	}
 
 }
