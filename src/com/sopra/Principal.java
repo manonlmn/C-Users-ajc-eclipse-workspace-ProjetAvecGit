10b17a5e06413e1d292b0fff39ed5697a3f28c3c
Manon <lmn.manon@gmail.com>
2017-08-08 14:48:23 +0200

    Modification Classe principale

--- src/com/sopra/Principal.java
+++ src/com/sopra/Principal.java
@@ -6,7 +6,9 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Tadam");
 		// A NE PAS EXECUTER
-		for(;;) {}
+		for(int i=0;i<10;i++) {
+			System.out.println("Tadam "+i+" !");
+		}
 	}
 
 }
