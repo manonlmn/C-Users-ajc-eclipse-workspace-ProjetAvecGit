b60c5e17c595d7800e849d39cc791c735db0f15b
gonzagueducrocq <gonzague.ducrocq@soprasteria.com>
2017-08-08 16:18:24 +0200

    Auto stash before merge of "Front" and "origin/Front"

--- src/com/sopra/windows/Panel3.java
+++ src/com/sopra/windows/Panel3.java
@@ -1,11 +1,18 @@
 package com.sopra.windows;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panel3 extends JPanel {
 
-
+	
+	private JLabel CheckBox = new JLabel("10 de der."); 
+	private JCheckBox checkBox = new JCheckBox();
+	
+	private JButton BoutonAjouter = new JButton("Ajouter");
+	private JButton BoutonReset = new JButton("Reset");
+	private JButton BoutonCalcul = new JButton("Calcul");
 	
 }
--- src/com/sopra/windows/Panel4.java
+++ src/com/sopra/windows/Panel4.java
@@ -1,7 +1,9 @@
 package com.sopra.windows;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panel4 extends JPanel {
 
+	private JLabel ListeScores = new JLabel("Liste des Scores"); 
 }
